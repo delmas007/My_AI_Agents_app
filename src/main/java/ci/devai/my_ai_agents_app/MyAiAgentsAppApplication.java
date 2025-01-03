b3dde9a4683357215ec6ef28b5b10ddf7cb79852
delmas <azertyazerty1ze@gmail.com>
2025-01-03 22:27:14 +0000

    init

---
+++ src/main/java/ci/devai/my_ai_agents_app/MyAiAgentsAppApplication.java
@@ -0,0 +1,13 @@
+package ci.devai.my_ai_agents_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAiAgentsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAiAgentsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ci/devai/my_ai_agents_app/MyAiAgentsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.devai.my_ai_agents_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAiAgentsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
